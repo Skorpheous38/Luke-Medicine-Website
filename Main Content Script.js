2dd9593b954b9c1fe9e70ca1f66ae823897a0771
Joshua Esclamado <113429401+codingJoshua123@users.noreply.github.com>
2024-08-12 14:44:19 +0800

    Add files via upload

--- Main Content Script.js
+++ Main Content Script.js
@@ -1,15 +1,29 @@
 //An array of WEBP image files
 const slide_show_images = [image1.webp, image2.webp, image3.webp, image4.webp, image5.webp];
 
-//Next Button
-function next_button() {
-    var current_image;
-    document.getElementById("image_here").innerHTML = slide_show_images[current_image++];
+let current_index = 0;
+const theImageDisplay = document.getElementById('image_here');
+const theDecrementButton = document.getElementById('previousButton');
+const theIncrementButton = document.getElementById('nextButton');
+
+//Image Display Function
+function display_image() {
+    theImageDisplay.textContent = current_index;
 }
 
+//Next Button
+theDecrementButton.addEventListener('click', () => {
+    if (current_index > 0) {
+        current_index--;
+        display_image();
+    }
+});
+
 //Previous Button
-function previous_button() {
-    var current_image;
-    document.getElementById("image_here").innerHTML = slide_show_images[current_image--];
-}
+theIncrementButton.addEventListener('click', () => {
+    if (current_index > slide_show_images.length - 1) {
+        current_index++;
+        display_image();
+    }
+});
 
