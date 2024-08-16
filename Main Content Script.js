8f39f45d40af7f1ca4fe6a31dc18e4252e8abef4
Joshua Esclamado <113429401+codingJoshua123@users.noreply.github.com>
2024-08-16 19:14:30 +0800

    Add files via upload

--- Main Content Script.js
+++ Main Content Script.js
@@ -1,29 +1,23 @@
 //An array of WEBP image files
-const slide_show_images = [image1.webp, image2.webp, image3.webp, image4.webp, image5.webp];
+const slide_show_images = [image1.jfif, image2.jfif, image3.jfif, image4.jpeg, image5.jpeg];
 
 let current_index = 0;
-const theImageDisplay = document.getElementById('image_here');
+const theImageDisplay = document.getElementById('image_container');
 const theDecrementButton = document.getElementById('previousButton');
 const theIncrementButton = document.getElementById('nextButton');
 
 //Image Display Function
-function display_image() {
-    theImageDisplay.textContent = current_index;
+function display_image(let refered_image) {
+    theImageDisplay.innerHTML = refered_image;
 }
 
-//Next Button
-theDecrementButton.addEventListener('click', () => {
-    if (current_index > 0) {
-        current_index--;
-        display_image();
-    }
-});
-
-//Previous Button
-theIncrementButton.addEventListener('click', () => {
-    if (current_index > slide_show_images.length - 1) {
-        current_index++;
-        display_image();
-    }
-});
-
+function previous_button() {
+    let decrease = current_index--;
+    let the_element = slide_show_images[decrease];
+    display_image();
+}
+function next_button() {
+    let increase = current_index++;
+    let the_element = slide_show_images[increase];
+    display_image();
+}
