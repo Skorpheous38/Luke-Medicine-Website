f9ba155bd7896f307c883d6728bf0fed9d63f489
Joshua Esclamado <113429401+codingJoshua123@users.noreply.github.com>
2024-08-17 18:05:00 +0800

    Add files via upload

---
+++ Luke_Medicine_Website_Server.php
@@ -0,0 +1,20 @@
+<? php 
+
+//Retrieve the data from the user
+
+$first_name = $_REQUEST['fname'];
+$last_name = $_REQUEST['lname'];
+$middle_name = $_REQUEST['midname'];
+$user_age = $_REQUEST['age2'];
+$user_gender = $_REQUEST['genderselect'];
+$birth_date = $_REQUEST['birthdate'];
+$phone_number = $_REQUEST['phonenumber'];
+$user_email = $_REQUEST['useremail'];
+$zone_purok = $_REQUEST['zone_or_purok2'];
+$barangay = $_REQUEST['barangay2'];
+$province_city = $_REQUEST['province_or_city2'];
+$have_medical_card = $_REQUEST['haveMedicalCard'];
+$user_medical_card = $_REQUEST['medicalCard'];
+
+
+?>
