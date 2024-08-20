668e516301fdfef90476ae9b3ef70e8ebf01e0c4
Joshua Esclamado <113429401+codingJoshua123@users.noreply.github.com>
2024-08-20 13:55:26 +0800

    Add files via upload

--- Luke_Medicine_Website_Server.php
+++ Luke_Medicine_Website_Server.php
@@ -1,5 +1,181 @@
 <? php 
 
+function record_customer() {
+
+    $customer_record = [];
+
+    $first_name = "INSERT INTO Customer(Firstname) VALUES ($_POST['fname'])";
+    $last_name = "INSERT INTO Customer(Lastname) VALUES ($_POST['lname'])";
+    $middle_name = "INSERT INTO Customer(Middlename) VALUES ($_POST['midname'])";
+    $user_age = "INSERT INTO Customer(Age) VALUES ($_POST['age'])";
+    $user_gender = "INSERT INTO Customer(Gender) VALUES ($_POST['genderselect'])";
+    $phone_number = "INSERT INTO Customer(Phonenumber) VALUES ($_POST['phonenumber'])";
+
+    array_push($customer_record, $first_name, $last_name, $middle_name, $user_age, $user_gender, $phone_number);
+    
+    foreach ($customer_record as $variable) {
+        if ($connect_server -> query($variable) === TRUE) {
+
+            echo "$variable is inserted." . "\n";
+
+        } else {
+
+            echo "$variable is rejected." . "\n" . $connect_server -> error;
+
+        }
+    }
+    
+}
+
+//Database Admin Test Sign In
+
+$server_name = "localhost";
+$username = "Joshua Esclamado";
+$server_access_password = "\$cosmological12$%";
+$database_name = "Luke_Medicine_Website";
+
+//Connect to mySQL Server
+
+$connect_server = new mysqli($server_name, $username, $server_access_password, $database_name);
+if ($connect_server -> connect_error) {
+    die("Connection failed: " . $connect_server -> connect_error);
+}
+echo "Connected to Server successfully!";
+$database = "CREATE DATABASE Luke_Medicine_Pharmacy";
+if ($connect_server -> query($database) === TRUE ) {
+    echo "Database is created!";
+} else {
+    echo "Database Connection Error:" . $connect_server -> error;
+}
+
+$table1 = " 
+CREATE TABLE Customer (
+    Customer_ID int NOT NULL AUTO_INCREMENT,
+    Firstname varchar(50) NOT NULL,
+    Lastname varchar(50) NOT NULL,
+    Middlename varchar(50) NOT NULL,
+    Phonenumber int NOT NULL,
+    Age int NOT NULL,
+    Gender varchar(6) NOT NULL,
+    Branch_ID int NOT NULL,
+    Item_ID int NOT NULL,
+
+    PRIMARY KEY(Customer_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Pharmacy(Branch_ID),
+    FOREIGN KEY (Item_ID) REFERENCES 
+
+    UNIQUE (Customer_ID)
+)
+"
+$table2 = "
+CREAT TABLE Manager (
+    Manager_ID int NOT NULL AUTO_INCREMENT,
+    Firstname varchar(50) NOT NULL,
+    Lastname varchar(50) NOT NULL,
+    Middlename varchar(50) NOT NULL,
+    Phonenumber int NOT NULL,
+    Age int NOT NULL,
+    Gender varchar(6) NOT NULL,
+    Branch_ID int NOT NULL,
+    Pharmacist_ID int NOT NULL
+
+    PRIMARY KEY (Manager_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Pharmacy(Branch_ID),
+    FOREIGN KEY (Pharmacist_ID) REFERENCES Pharmacist(Pharmacist_ID)
+
+    UNIQUE (Manager_ID)
+)
+"
+$table3 = "
+CREATE TABLE Pharmacist(
+    Pharmacist_ID int NOT NULL AUTO_INCREMENT,
+    Firstname varchar(50) NOT NULL,
+    Lastname varchar(50) NOT NULL,
+    Middlename varchar(50) NOT NULL,
+    Gender varchar(6) NOT NULL,
+    Age int NOT NULL,
+    Branch_ID int NOT NULL,
+    Manager_ID int NOT NULL,
+    Item_ID int NOT NULL
+
+    PRIMARY KEY (Pharmacist_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Pharmacy(Branch_ID),
+    FOREIGN KEY (Manager_ID) REFERENCES Manager(Manager_ID),
+    FOREIGN KEY (Item_ID) REFERENCES Stock_House(Item_ID)
+
+    UNIQUE (Pharmacist_ID)
+)
+"
+$table4 = "
+CREATE TABLE Pharmacy (
+    Branch_ID int NOT NULL AUTO_INCREMENT,
+    Manager_ID int NOT NULL,
+    Pharmacist_ID int NOT NULL,
+    Item_ID int NOT NULL,
+    Customer_ID int NOT NULL
+
+    PRIMARY KEY (Branch_ID),
+    FOREIGN KEY (Manager_ID) REFERENCES Manager(Manager_ID),
+    FOREIGN KEY (Pharmacist_ID) REFERENCES Pharmacist(Pharmacist_ID),
+    FOREIGN KEY (Item_ID) REFERENCES Stock_House(Item_ID),
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+
+    UNIQUE (Branch_ID)
+)
+"   
+$table5 = "
+CREATE TABLE Stock_House(
+    Item_ID int NOT NULL AUTO_INCREMENT,
+    Branch_ID int NOT NULL
+
+    PRIMARY KEY (Item_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Pharmacy(Branch_ID)
+
+    UNIQUE (Item_ID)
+)
+"
+$table6 = "
+CREATE TABLE Transaction (
+    Transaction_ID int NOT NULL AUTO_INCREMENT,
+    Purchase_Date_Time DATETIME NOT NULL,
+    Branch_ID int NOT NULL
+
+    PRIMARY KEY (Transaction_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Pharmacy(Branch_ID)
+
+    UNIQUE (Transaction_ID)
+)
+"
+$array_of_tables = array($table1, $table2, $table3, $table4, $table5, $table6);
+
+foreach ($array_of_tables as $variable) {
+
+    if ($connect_server -> query($variable) === TRUE) {
+        echo "Table $variable is successfuly created. ";
+    } else {
+        echo "Failed to create $variable" . $connect_server -> error;
+    }
+
+}
+    
+
+
+//Obtain Form Data
+
+$first_name2 = $_POST['fname'];
+$last_name2 = $_POST['lname'];
+$middle_name2 = $_POST['midname'];
+$user_age2 = $_POST['age'];
+$user_gender2 = $_POST['genderselect'];
+$birth_date2 = $_POST['birthdate'];
+$phone_number2 = $_POST['phonenumber'];
+$user_email2 = $_POST['userEmail'];
+$zone_purok2 = $_POST['zone_or_purok'];
+$barangay2 = $_POST['barangay'];
+$province_city2 = $_POST['province_or_city'];
+$have_medical_card2 = $_POST['haveMedicalCard'];
+$user_medical_card2 = $_POST['medicalCard'];
+
 
 
 //Server Specification
@@ -58,20 +234,5 @@ $have_medical_card = $_REQUEST['haveMedicalCard'];
 $user_medical_card = $_REQUEST['medicalCard'];
 
 
-
-//Obtain Form Data
-
-$first_name2 = $_POST['fname'];
-$last_name2 = $_POST['lname'];
-$middle_name2 = $_POST['midname'];
-$user_age2 = $_POST['age'];
-$user_gender2 = $_POST['genderselect'];
-$birth_date2 = $_POST['birthdate'];
-$phone_number2 = $_POST['phonenumber'];
-$user_email2 = $_POST['userEmail'];
-$zone_purok2 = $_POST['zone_or_purok'];
-$barangay2 = $_POST['barangay'];
-$province_city2 = $_POST['province_or_city'];
-$have_medical_card2 = $_POST['haveMedicalCard'];
-$user_medical_card2 = $_POST['medicalCard'];
+$connect_server -> close();
 ?>
